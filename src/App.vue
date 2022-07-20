9928690354ffa591a79faf92b478403b8b9cc69e
mjjin <jmjgeminix@gmail.com>
2022-07-20 17:15:52 +0800

    test tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-  <router-view></router-view>
+  <div class="p-4">
+    <router-link to="/" class="text-white bg-blue-500 rounded border px-4 py-2"
+      >Home</router-link
+    >
+    <router-link
+      to="/about"
+      class="text-white bg-blue-500 rounded border px-4 py-2"
+      >About</router-link
+    >
+    <router-view></router-view>
+  </div>
 </template>
